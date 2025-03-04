619577fa3c3d34ebec6c825c2e15318542b2d189
Janith Sameera <janeesameera@gmail.com>
2025-03-04 15:50:29 +0530

    loan view

--- resources/views/pages/LoanView.blade.php
+++ resources/views/pages/LoanView.blade.php
@@ -152,7 +152,7 @@
                                     <th scope="col">Loan Stock</th>
                                     <th scope="col">Loan Portfolio</th>
                                     <th scope="col">Total Paid Amount</th>
-                                    <th scope="col">Saving Amount</th>
+                                    <th scope="col">Scheduled Saving Amount</th>
                                     <th scope="col">Saving Balance</th>
                                     <th scope="col">Total Balance</th>
                                     <th scope="col">Capital Balance</th>
