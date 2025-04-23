c5f43e22b5ef703d5e1579ad929bf239ef82d96c
Janith Sameera <janeesameera@gmail.com>
2025-04-23 10:49:40 +0530

    KYC and loan view

--- resources/views/pages/Insurance/KYC.blade.php
+++ resources/views/pages/Insurance/KYC.blade.php
@@ -149,7 +149,7 @@
     <!-- Select2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
+        document.addEvaentListener("DOMContentLoaded", function () {
             const selectedId = "{{ $id ?? 0 }}";
             const customerSelect = document.getElementById("customerSelect");
 
