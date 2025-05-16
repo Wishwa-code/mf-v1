93f54ccd4a6918a94eaefc2d566c94c6bc844905
Janith Sameera <janeesameera@gmail.com>
2025-05-16 16:51:07 +0530

    403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.admin')
+
+@section('content')
+    <div class="text-center mt-5">
+        <h1 class="text-danger">403 - Access Denied</h1>
+        <p>You do not have permission to access this page.</p>
+        <a href="/" class="btn btn-primary">Go Home</a>
+    </div>
+@endsection
