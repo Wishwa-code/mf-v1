ca1facadc6e367db45449fa53d95c670ae321e23
Janith Sameera <janeesameera@gmail.com>
2025-07-07 16:52:18 +0530

    bug fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -342,8 +342,11 @@ class UserController extends Controller
         $setteled_loan_Count = tableWithBranch('customer_loan')->where('Status','=','1')->count();
         $deleted_loan_Count = tableWithBranch('customer_loan')->where('Status','=','-2')->count();
         $setteled_loan_current_Amount = tableWithBranch('customer_loan')->where('Status','=','1')->sum('Amount');
-        $todayinstallment = tableWithBranch('installments')->where('Installment_Date',date('Y-m-d'))->where('Status','=','0')->sum('Total_Balance');
-
+        $todayinstallment = tableWithBranch('customer_loan', 'customer_loan')
+            ->join('installments', 'customer_loan.idCustomer_Loan', '=', 'installments.Customer_Loan_idCustomer_Loan')
+            ->where('installments.Installment_Date', '=', date('Y-m-d'))
+            ->where('customer_loan.Status', '=', 0)
+            ->sum('installments.Total_Balance');
         $todaycollected = tableWithBranch('customer_payments')->where('Date',date('Y-m-d'))->sum('Amount');
 
 
