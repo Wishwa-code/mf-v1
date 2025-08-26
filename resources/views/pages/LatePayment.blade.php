611d5bbe62564ceb81997aff4b0a5635e4dc9d08
Janith Sameera <janeesameera@gmail.com>
2025-08-27 02:34:53 +0530

    report fix

--- resources/views/pages/LatePayment.blade.php
+++ resources/views/pages/LatePayment.blade.php
@@ -268,9 +268,9 @@
                                     <th>Installment Amount</th>
                                     <th>Pending Installments</th>
                                     <th>Penalty Total</th>
-                                    <th>Pending Total</th>
                                     <th>Loan Balance</th>
                                     <th>Capital Balance</th>
+                                    <th>Total Balance</th>
                                     <th>Status</th>
                                     <th>Action</th>
                                 </tr>
@@ -544,12 +544,12 @@
                             <td>${parseFloat(item.Installment_Amount).toFixed(2)}</td>
                             <td>${item.Installment_Count}</td>
                             <td>${parseFloat(item.Panalty_Balance).toFixed(2)}</td>
-                            <td>${parseFloat(item.Total_Balance).toFixed(2)}</td>
-                            <td>${(
-                                (parseFloat(item.Total_Balance) || 0) +
+                            <td>${parseFloat(item.Balance_Amount).toFixed(2)}</td>
+                            <td>${parseFloat(item.capital_balance).toFixed(2)}</td>
+<td>${(
+                                (parseFloat(item.Balance_Amount) || 0) +
                                 (parseFloat(item.Panalty_Balance) || 0)
                             ).toFixed(2)}</td>
-                            <td>${parseFloat(item.capital_balance).toFixed(2)}</td>
                             <td><i class="fas fa-lightbulb bulb-icon" style="color: ${statusColor}"></i></td>
                             <td><a href="/loanview/${item.idCustomer_Loan}" target="_blank" class="btn btn-warning"><i class="bi bi-eye"></i></a></td>
                         </tr>
