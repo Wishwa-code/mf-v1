6679802dee292f87431b4cf80fa1fbc425277087
malithonline <120547828+malithonline@users.noreply.github.com>
2025-08-30 16:57:02 +0530

    reduce hover effect speed for branch switch

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -174,8 +174,8 @@
         left: -100%;
         width: 100%;
         height: 100%;
-        background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.2), transparent);
-        transition: left 0.5s;
+        background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.15), transparent);
+        transition: left 1.2s ease-in-out;
     }
     
     .modern-dropdown-toggle:hover::before {
