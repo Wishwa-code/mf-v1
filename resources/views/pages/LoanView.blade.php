13fd7efbba1acb5ad9c43eb7fa6e5b93830ac6c4
malithonline <120547828+malithonline@users.noreply.github.com>
2025-09-17 17:41:14 +0530

    add saving bal to loan view

--- resources/views/pages/LoanView.blade.php
+++ resources/views/pages/LoanView.blade.php
@@ -359,6 +359,7 @@
 
                                     <th scope="col">Total Paid Penalty</th>
                                     <th scope="col">Total Penalty Balance</th>
+                                    <th scope="col">Saving Balance</th>
                                     <th scope="col">Capital Balance</th>
                                     <th scope="col">Interest Balance</th>
                                     <th scope="col">Total Outstanding</th>
@@ -376,6 +377,7 @@
 
                                     <td>{{ number_format($Panalty_Amount-$Panalty_BalanceSum, 2, '.', ',') }}</td>
                                     <td>{{ number_format($Panalty_BalanceSum, 2, '.', ',') }}</td>
+                                    <td>{{ number_format($savingBalanceSum, 2, '.', ',') }}</td>
                                     <td>{{ number_format($loan->capital_balance, 2, '.', ',') }}</td>
                                     <td>{{ number_format($loan->installment_balance, 2, '.', ',') }}</td>
                                     <td>{{ number_format($loan->Total_Loan_Amount-($total_paid_amount-$savingBalanceSum)+$Panalty_BalanceSum+($Panalty_Amount-$Panalty_BalanceSum), 2, '.', ',') }}</td>
