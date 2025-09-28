1426ed67201b995653e2dea9ac75672424e596d6
malithonline <120547828+malithonline@users.noreply.github.com>
2025-09-28 13:12:54 +0530

    showcustomers -  remove doc upload for head office

--- resources/views/pages/ViewCustomer.blade.php
+++ resources/views/pages/ViewCustomer.blade.php
@@ -58,12 +58,15 @@
                         <div class="d-flex flex-column align-items-start mb-3">
                             <h4 class="page-title mb-3">Customer Details</h4>
 
+                            @php $isHeadOffice = session('branch_id') == -1; @endphp
+                            @if(!$isHeadOffice)
                             <!-- File input -->
                             <label style="color: red">Upload Excel</label>
                             <input type="file" id="uploadExcel" accept=".xlsx, .xls" class="form-control mb-2 w-50">
 
                             <!-- Upload button, aligned below the file input -->
                             <input type="button" onclick="upload_excel()" class="btn btn-success mt-2" value="Upload">
+                            @endif
                         </div>
 
 
