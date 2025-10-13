4948309ca4792657c905e5e1b35c37e39b654945
malithonline <120547828+malithonline@users.noreply.github.com>
2025-10-13 10:33:18 +0530

    HO - fixed save expenses error

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -356,15 +356,19 @@ class ReportController extends Controller
                     ->where('acc_type_group','=','Expenses')
                     ->where('Idbank','=',$request->category)
                     ->first();
-                $this->bankLogController->index($request->bank,"Expenses",$reason,"-","credit",$amount,$bank_id->Idbank);
-                $this->bankLogController->index($bank_id->Idbank,"Expenses",$reason,"-","debit",$amount,$request->bank);
+                if ($bank_id) {
+                    $this->bankLogController->index($request->bank,"Expenses",$reason,"-","credit",$amount,$bank_id->Idbank);
+                    $this->bankLogController->index($bank_id->Idbank,"Expenses",$reason,"-","debit",$amount,$request->bank);
+                }
             }else{
                 $bank_id=tableWithBranch('company_bank_accounts')
                     ->where('acc_type_group','=','Income')
                     ->where('Idbank','=',$request->category)
                     ->first();
-                $this->bankLogController->index($request->bank,"Income",$reason,"-","debit",$amount,$bank_id->Idbank);
-                $this->bankLogController->index($bank_id->Idbank,"Income",$reason,"-","credit",$amount,$request->bank);
+                if ($bank_id) {
+                    $this->bankLogController->index($request->bank,"Income",$reason,"-","debit",$amount,$bank_id->Idbank);
+                    $this->bankLogController->index($bank_id->Idbank,"Income",$reason,"-","credit",$amount,$request->bank);
+                }
             }
 
 
