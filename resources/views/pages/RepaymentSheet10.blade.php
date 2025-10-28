f1cd059cc109c347d6e7cb836271c294e66d802f
malithonline <120547828+malithonline@users.noreply.github.com>
2025-10-28 15:28:27 +0530

    HO - left align member name

--- resources/views/pages/RepaymentSheet10.blade.php
+++ resources/views/pages/RepaymentSheet10.blade.php
@@ -338,7 +338,9 @@
                                     @foreach($group as $item)
                                         <tr class="group-row">
                                             <td>{{ $item->cus_number }}</td>
-                                            <td class="fixed-name">{{ format_member_name($item->customer_first, $item->customer_lastname, $name_mode ?? 'with_initial') }}</td>
+                                            <td class="fixed-name" style="text-align: left;">
+                                                {{ format_member_name($item->customer_first, $item->customer_lastname, $name_mode ?? 'with_initial') }}
+                                            </td>
                                             <td>{{ number_format($item->Loan_Amount, 2) }}</td>
                                             <td>{{ number_format($item->Installment_Amount, 2) }}</td>
                                             <td>{{ number_format($item->Balance_Amount, 2) }}</td>
