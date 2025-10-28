ef0ced97421c40c14e27374143c188313cab1edd
malithonline <120547828+malithonline@users.noreply.github.com>
2025-10-28 16:58:01 +0530

    sheet10 - fixed total misplace

--- resources/views/pages/RepaymentSheet10.blade.php
+++ resources/views/pages/RepaymentSheet10.blade.php
@@ -369,20 +369,6 @@
                                         <td>{{ number_format($group->sum('Balance_Amount'), 2) }}</td>
                                         <td colspan="13"></td>
                                     </tr>
-                                    {{-- Page Total Row --}}
-                                    <tr class="group-row" style="font-weight: bold;">
-                                        <td colspan="2">Page Total</td>
-                                        @for ($k = 0; $k < 16; $k++)
-                                            <td></td>
-                                        @endfor
-                                    </tr>
-                                    {{-- Page Due Amount Row --}}
-                                    <tr class="group-row" style="font-weight: bold;">
-                                        <td colspan="2">Page Due Amount</td>
-                                        @for ($k = 0; $k < 16; $k++)
-                                            <td></td>
-                                        @endfor
-                                    </tr>
                                 @endforeach
                                 {{-- Fill remaining groups to make 5 total --}}
                                 @for ($g = $groupIndex; $g < $totalGroupsToShow; $g++)
@@ -403,21 +389,21 @@
                                         <td>0.00</td>
                                         <td colspan="13"></td>
                                     </tr>
-                                    {{-- Page Total Row --}}
-                                    <tr class="group-row" style="font-weight: bold;">
-                                        <td colspan="2">Page Total</td>
-                                        @for ($k = 0; $k < 16; $k++)
-                                            <td></td>
-                                        @endfor
-                                    </tr>
-                                    {{-- Page Due Amount Row --}}
-                                    <tr class="group-row" style="font-weight: bold;">
-                                        <td colspan="2">Page Due Amount</td>
-                                        @for ($k = 0; $k < 16; $k++)
-                                            <td></td>
-                                        @endfor
-                                    </tr>
                                 @endfor
+                                {{-- Page Total Row (once per page after all groups) --}}
+                                <tr style="font-weight: bold; background-color: #f2f2f2;">
+                                    <td colspan="2">Page Total</td>
+                                    @for ($k = 0; $k < 16; $k++)
+                                        <td></td>
+                                    @endfor
+                                </tr>
+                                {{-- Page Due Amount Row (once per page after all groups) --}}
+                                <tr style="font-weight: bold; background-color: #f2f2f2;">
+                                    <td colspan="2">Page Due Amount</td>
+                                    @for ($k = 0; $k < 16; $k++)
+                                        <td></td>
+                                    @endfor
+                                </tr>
                                 {{-- Total Due Amount Row at the very end --}}
                                 <tr style="font-weight: bold; background-color: #f2f2f2;">
                                     <td colspan="2">Total Due Amount</td>
